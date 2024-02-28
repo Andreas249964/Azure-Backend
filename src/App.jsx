b5ca29eed85dff782329c4a9b6fcfcf84317931e
GithubKolden <122537835+GithubKolden@users.noreply.github.com>
2024-02-28 17:44:29 +0100

    update app.jsx with api call (unauthorized)

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,86 @@ import './App.css'
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal, MsalProvider } from '@azure/msal-react'
 import { loginRequest } from './auth-config'
 
+const CallAPI = (instance, accounts, setApiData) => {
+    // Silently acquires an access token which is then attached to a request for API call
+    instance.acquireTokenSilent({
+        ...loginRequest,
+        account: accounts[0]
+    }).then((response) => {
+        console.log(response.accessToken);
+
+        fetch('https://getallcards.azurewebsites.net', { 
+            method: 'post', 
+            headers: new Headers({
+                'Authorization': 'Bearer ' + response.accessToken,
+                'Accept': 'application/json'
+            })
+        })
+        
+        .then(data => data.json())
+        .then(json => {
+            console.log(json);
+            setApiData(json);
+        });
+    });
+}
+
+const AppContent = () => {
+    
+    const { instance, accounts } = useMsal();
+    const [apiData, setApiData] = useState(null);
+
+    return (
+        <div>
+            <p>App Content</p>
+            {/* You can include components or other content here */}
+        </div>
+    );
+}
+
 const WrappedView = () => {
 
-  const { instance } = useMsal();
-  const activeAccount =instance.getActiveAccount();
+    const { instance } = useMsal();
+    const activeAccount = instance.getActiveAccount();
+    const [apiData, setApiData] = useState(null);
 
-  const handleRedirect = () => {
-      instance
-      .loginRedirect({
-        ...loginRequest,
-        prompt: "create",
-      })
-      .catch((error) => console.log(error))
-  }
+    const handleRedirect = () => {
+        instance
+        .loginRedirect({
+          ...loginRequest,
+          prompt: "create",
+        })
+        .catch((error) => console.log(error))
+    }
 
-  return (
-    <div className="App">
-      <AuthenticatedTemplate>
-        {activeAccount ? (
-          <p>
-            Authenticated Successfully
-          </p>
-        ) : null}
-      </AuthenticatedTemplate>
-      <UnauthenticatedTemplate>
-        <button onClick={handleRedirect}>
-          Sign up
-        </button>
-      </UnauthenticatedTemplate>
-    </div>
-  )
+    const handleCallAPI = () => {
+        CallAPI(instance, instance.getActiveAccount(), setApiData);
+    }
+
+    return (
+        <div className="App">
+            <AuthenticatedTemplate>
+                {activeAccount ? (
+                    <div>
+                        
+                        <p>Authenticated Successfully</p>
+                        <button onClick={handleCallAPI}>Call API</button>
+                    </div>
+                ) : null}
+            </AuthenticatedTemplate>
+            <UnauthenticatedTemplate>
+                <button onClick={handleRedirect}>Sign up</button>
+            </UnauthenticatedTemplate>
+        </div>
+    );
 }
 
 const App = ({ instance }) => {
-  return (
-    <MsalProvider instance={instance}>
-        <WrappedView />
-    </MsalProvider>
-  )
+    return (
+        <MsalProvider instance={instance}>
+            <WrappedView />
+        </MsalProvider>
+    );
 }
 
-export default App
+export default App;
